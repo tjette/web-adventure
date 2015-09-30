97363d40cdaebf527da26d36b8bef926ae07abcd
Zachary Warren <zach@Zacharys-MacBook-Pro.local>
2015-09-30 09:30:13 -0600

    Added winning condition and reset button

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var yCoord = 0;
 var xCoord = 0;
+
+// Initializing the corrdinates of the target cell 
+var endX = 2;
+var endY = 2;
+
+//Adding a function to replace the winning cell with a picture of dead Abe Simpson
+var killAbe = function () {
+	document.getElementById(endY.toString() + endX.toString()).innerHTML = "<img src=\"dead.jpg\"></img>";
+};
+
+//Adding a function to place an image at the target cell
+var target = function(){
+	document.getElementById(endY.toString() + endX.toString()).innerHTML = "<img src=\"retire.png\"></img>";
+};
+    
 var movAbe = function(){
 	document.getElementById(yCoord.toString() + xCoord.toString()).innerHTML = "<img src=\"small_abe.png\"></img>";
 };
@@ -19,18 +34,30 @@ var hideAbeRight = function(){
 	document.getElementById(yCoord.toString() + (xCoord - 1).toString()).innerHTML = "";
 };
 
+//Adding a function to delete abe from the current cell when 'reset' is pressed
+var hideAbeNow = function(){
+	document.getElementById(yCoord.toString() + xCoord.toString()).innerHTML = "";
+};
+
 movAbe();
+target();
 
 
 
 function movDown() {
 	
 	
-	if (yCoord === 2){
-		alert("D'oh!!!");
+	if(xCoord === endX && yCoord === (endY - 1)) {
+        yCoord++;
+        hideAbeDown();	
+        killAbe();
+        alert("Whuuthaa!!??");
 	}
-	else {
-		yCoord++;
+	else if (yCoord === 2) {
+        alert("D'oh!!!");
+    }
+        else {
+        yCoord++;
 		hideAbeDown();
 		movAbe();
 	};
@@ -38,8 +65,11 @@ function movDown() {
 
 function movUp() {
 	
-	
-	if (yCoord === 0){
+	if(xCoord === endX && yCoord === endY) {
+        killAbe();
+	   alert("WhuuThe!!??");
+    }
+	else if (yCoord === 0){
 		alert("D'oh!!!");
 	}
 	else {
@@ -51,8 +81,11 @@ function movUp() {
 
 function movLeft() {
 	
-	
-	if (xCoord === 0){
+	if(xCoord === endX && yCoord === endY) {
+        killAbe();
+	   alert("WhuuThe!!??");
+    }
+	else if (xCoord === 0){
 		alert("D'oh!!!");
 	}
 	else {
@@ -64,14 +97,26 @@ function movLeft() {
 
 function movRight() {
 	
-	
-	if (xCoord === 2){
+	if(xCoord === (endX - 1) && yCoord === endY) {
+        xCoord++;
+        hideAbeRight();
+        killAbe();
+	   alert("WhuuThe!!??");
+    }
+	else if (xCoord === 2){
 		alert("Don't have cow, man!");
 	}
 	else {
-		
-		xCoord++;hideAbeRight();
+		xCoord++;
+        hideAbeRight();
 		movAbe();
 	};
 }
 
+function reset() {
+    hideAbeNow();
+    xCoord = 0;
+    yCoord = 0;
+    movAbe();
+    target();
+}
